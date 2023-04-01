cb2180dc753f48613ceaadfcc00be4272e3ff606
DEVROLOBE <lopezbedonroxana@gmail.com>
2023-04-01 18:21:47 -0500

    agregando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='header'>Header</header>
+    <header className='header'>
+      <div className="container">
+        <nav>
+        <Link to="/">
+          <img className='logo' src="/icon-logo.png" alt="LOGO DE FISIONFULNESS" />
+        </Link>
+          <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/quienes-somos">Quiénes Somos</Link></li>
+            <li><Link to="/servicios">Servicios</Link></li>
+            <li><Link to="/trabaja-con-nosotros">Trabaja con Nosotros</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
+
+
+
+
