afab6f7c5074a327d3a8a8b55a01cd036a1f60d7
frontend-antony <elmer.anthony12@gmail.com>
2023-04-08 23:39:25 -0500

    cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, NavLink } from 'react-router-dom';
+import { BiHomeAlt2, BiUser, BiBriefcaseAlt, BiCog, BiGridAlt } from 'react-icons/bi';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
-    document.body.style.overflow = !isOpen ? "hidden" : "";
+
+    if (!isOpen) {
+      document.body.classList.add("header-navbar");
+    } else {
+      document.body.classList.remove("header-navbar");
+    }
   };
 
   return (
-    <header className={`header ${isOpen ? 'active' : ''}`}>
+    <header className="header">
       <div className="container">
         <nav>
           <Link to="/" className='logo'>
@@ -19,56 +25,63 @@ const Header = () => {
           
           <div className='navbar'>
             <ul>
-              <li>
-                <NavLink 
-                  to="/"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Inicio
-                </NavLink>
-              </li>
+              <div className="text-zinc-500 font-bold text-xs uppercase mt-[16px] mb-[12px] hide-for-desktop">Menú</div>
 
-              <li>
-                <NavLink 
-                  to="/quienes-somos"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Quiénes Somos
-                </NavLink>
-              </li>
-              
-              <li>
-                <NavLink 
-                  to="/servicios"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Servicios
-                </NavLink>
-              </li>
-              
-              <li>
-                <NavLink 
-                  to="/trabaja-con-nosotros"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Trabaja con Nosotros
-                </NavLink>
-              </li>
-              
-              <li>
-                <NavLink 
-                  to="/blog"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Blog
-                </NavLink>
-              </li>
+              <NavLink 
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                <div className="icon hide-for-desktop">
+                  <BiHomeAlt2 />
+                </div>
+                Inicio
+              </NavLink>
+
+              <NavLink 
+                to="/quienes-somos"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                <div className="icon hide-for-desktop">
+                  <BiUser />
+                </div>
+                Quiénes Somos
+              </NavLink>
+            
+              <NavLink 
+                to="/servicios"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                <div className="icon hide-for-desktop">
+                  <BiCog />
+                </div>
+                Servicios
+              </NavLink>
+            
+              <NavLink 
+                to="/trabaja-con-nosotros"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                <div className="icon hide-for-desktop">
+                  <BiBriefcaseAlt />
+                </div>
+                Trabaja con Nosotros
+              </NavLink>
+            
+              <NavLink 
+                to="/blog"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                <div className="icon hide-for-desktop">
+                  <BiGridAlt />
+                </div>
+                Blog
+              </NavLink>
             </ul>
           </div>
 
-          <button type='button' className={`ham-burger hide-for-desktop ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
-            <span></span>
-            <span></span>
+          {/* ham-burger */}
+
+          <button className={`ham-burger hide-for-desktop ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
             <span></span>
           </button>
         </nav>
