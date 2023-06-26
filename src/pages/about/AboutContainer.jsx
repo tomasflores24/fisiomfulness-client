43b30178f8875274dece8d6a1c5d64f9e80f2a61
maurobenitez <maurogabrielbenitez@gmail.com>
2023-06-26 15:01:26 -0300

    correcciones añadidas

--- src/pages/about/AboutContainer.jsx
+++ src/pages/about/AboutContainer.jsx
@@ -9,7 +9,7 @@ const AboutContainer = () => {
             <div className="flex items-center justify-center h-40">
               <img src="/icon-logo.png" alt="Logo" className="h-20" />
             </div>
-            <div>
+            <div className="mt-10">
               <img src="/images/about-1.jpg" alt="" className="w-full h-full object-cover rounded-2xl" />
             </div>
           </div>
@@ -19,7 +19,7 @@ const AboutContainer = () => {
               <p className="text-center text-2xl uppercase">Quienes somos</p>
             </div>
             <div className="p-4">
-              <h2 className="text-[#333] text-4xl font-bold tracking-[-.04em] mb-[24px] md:text-5xl">Más allá de lo farmacológico.</h2>
+              <h2 className="text-center text-[#333] text-4xl font-bold tracking-[-.04em] mb-[24px] md:text-5xl">Más allá de lo farmacológico</h2>
               <p className="text-lg text-[#444] tracking-[-.02em] max-w-[897px] mx-auto md:text-xl md:leading-[1.6]">En Fisiomfulness, estamos comprometidos a revolucionar el sector de la salud al proporcionar soluciones innovadoras y accesibles para mejorar la vida de las personas.<br></br>
                 Nos enorgullece ser una startup que busca marcar la diferencia y hacer del bienestar una prioridad para todos.<br></br><br></br>
                 El verdadero bienestar no se trata solo de estar físicamente en forma, sino también de encontrar el equilibrio en mente, cuerpo y espíritu.</p>
